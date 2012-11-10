cbfd8661ceec3a6ea69a2f867e6a9993ebe191dc
Dallas Read <dallas@excitereative.ca>
2012-11-10 11:42:07 -0400

    thats it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Blarg::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( shared.css admin.css admin.js )
+  config.assets.precompile += %w( shared.css shared.js admin.css admin.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
