7324fb9b7867c0d4d3efb886b7a779df552c5dae
Dallas Read <dallas@excitereative.ca>
2012-11-13 23:11:33 -0400

    go

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def current_site
-    @current_site ||= Site.find_by_permalink(request.subdomain)
+    if request.domain == "livehours.co"
+      @current_site ||= Site.find_by_permalink("livehours")
+    else
+      @current_site ||= Site.find_by_permalink(request.subdomain)
+    end
   end
   helper_method :current_site
   
